42d0f2f5a94ec2a41338766aaee16f584e0b40db
jiakang.cjk <jiakang.cjk@aliyun.com>
2021-03-16 19:04:10 +0800

    init

---
+++ build_gentoo_kernel.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+for ver in $@
+do
+  gcc -v && ld -v
+  cd /usr/src/linux-$ver-gentoo && \
+  make -j`cat /proc/cpuinfo | grep process | wc -l` && \
+  rm -rfv /lib/modules/*$ver* && \
+  make modules_install && \
+  rm -rfv /boot/*$ver* && \
+  make install && \
+  genkernel --install initramfs --kerneldir=/usr/src/linux-$ver-gentoo && \
+  cd / && rm -rfv /usr/src/kernel-$ver-gentoo-cjk-x86_64.tar.xz && \
+  tar -pcvf /usr/src/kernel-$ver-gentoo-cjk-x86_64.tar boot/config*$ver* boot/System*$ver* boot/vmlinuz*$ver* lib/modules/$ver-gentoo-cjk-x86_64 && \
+  xz -z9ev /usr/src/kernel-$ver-gentoo-cjk-x86_64.tar || (echo Failed in kernel: $ver && exit -1)
+done
+
+echo Complete built $@.
