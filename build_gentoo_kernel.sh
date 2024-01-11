4ef4e037596458e8a4cd42a2a5f4d4012e29981f
jiakang.cjk <jiakang.cjk@aliyun.com>
2024-01-11 11:47:47 +0800

    update kernel toolchain

--- build_gentoo_kernel.sh
+++ build_gentoo_kernel.sh
@@ -14,10 +14,10 @@ do
   gcc -v && ld -v
   cd /usr/src/linux-$ver-gentoo && \
   make clean && rm -rf target_dir && \
-  make CROSS_COMPILE=${CROSS_COMPILE} HOSTCC=${HOSTCC-gcc} -j`cat /proc/cpuinfo | grep process | wc -l` && \
+  make CROSS_COMPILE=${CROSS_COMPILE} HOSTCC="${HOSTCC-gcc} -fuse-ld=bfd" -j`cat /proc/cpuinfo | grep process | wc -l` && \
   mkdir -p target_dir/{boot,lib} && \
-  make CROSS_COMPILE=${CROSS_COMPILE} HOSTCC=${HOSTCC-gcc} INSTALL_MOD_PATH=./target_dir modules_install && \
-  make CROSS_COMPILE=${CROSS_COMPILE} HOSTCC=${HOSTCC-gcc} INSTALL_PATH=./target_dir/boot install && \
+  make CROSS_COMPILE=${CROSS_COMPILE} HOSTCC="${HOSTCC-gcc} -fuse-ld=bfd" INSTALL_MOD_PATH=./target_dir modules_install && \
+  make CROSS_COMPILE=${CROSS_COMPILE} HOSTCC="${HOSTCC-gcc} -fuse-ld=bfd" INSTALL_PATH=./target_dir/boot install && \
   genkernel initramfs --kernel-modules-prefix=./target_dir --kerneldir=/usr/src/linux-$ver-gentoo && \
   mv -v /boot/initramfs-$ver-gentoo*-cjk-${arch}.img ./target_dir/boot/ && \
   rm -rfv  ../kernel-$ver-gentoo-cjk-${arch}.tar.xz && \
