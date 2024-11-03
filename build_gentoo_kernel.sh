ae57ab6df86850c77fff133dc353b2976b5f1705
jiakang.cjk <jiakang.cjk@aliyun.com>
2024-11-03 23:01:58 +0800

    2.6.32 support gentoo

--- build_gentoo_kernel.sh
+++ build_gentoo_kernel.sh
@@ -11,6 +11,7 @@ esac
 
 for ver in $@
 do
+  [[ $ver < 3.0 ]] && nocompress="--no-compress-initramfs"
   gcc -v && ld -v
   mount -t tmpfs tmpfs /boot
   cd /usr/src/linux-$ver-gentoo && \
@@ -19,8 +20,9 @@ do
   mkdir -p target_dir/{boot,usr/lib} && ln -sv usr/lib target_dir/lib && \
   make CROSS_COMPILE=${CROSS_COMPILE} HOSTCC="${HOSTCC-gcc}" INSTALL_MOD_PATH=./target_dir modules_install && \
   make CROSS_COMPILE=${CROSS_COMPILE} HOSTCC="${HOSTCC-gcc}" INSTALL_PATH=./target_dir/boot install && \
-  genkernel initramfs --kernel-modules-prefix=./target_dir --kerneldir=/usr/src/linux-$ver-gentoo && \
-  mv -v /boot/initramfs-$ver-gentoo*-cjk-${arch}.img ./target_dir/boot/ && \
+  genkernel initramfs --kernel-modules-prefix=./target_dir --kerneldir=/usr/src/linux-$ver-gentoo $nocompress && \
+  if [[ $ver == 2.6.32.* ]]; then lzma -z9ev /boot/initramfs-$ver-gentoo-cjk-${arch}.img && mv /boot/initramfs-$ver-gentoo-cjk-${arch}.img.lzma /boot/initramfs-$ver-gentoo-cjk-${arch}.img; fi && \
+  mv -v /boot/initramfs-$ver-gentoo-cjk-${arch}.img ./target_dir/boot/ && \
   rm -rfv  ../kernel-$ver-gentoo-cjk-${arch}.tar.xz && \
   tar -pcf ../kernel-$ver-gentoo-cjk-${arch}.tar -C ./target_dir ./ && \
   xz -z9ev ../kernel-$ver-gentoo-cjk-${arch}.tar || (echo Failed in kernel: $ver && exit -1)
