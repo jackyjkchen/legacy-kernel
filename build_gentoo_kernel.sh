2c533dc55c207b5c47fbe38f5b02af56502425db
jiakang.cjk <jiakang.cjk@aliyun.com>
2025-09-21 17:09:45 +0800

    6.1/6.12: support loongson2f

--- build_gentoo_kernel.sh
+++ build_gentoo_kernel.sh
@@ -12,11 +12,11 @@ do
   make CROSS_COMPILE=${CROSS_COMPILE} HOSTCC="${HOSTCC-gcc}" -j`cat /proc/cpuinfo | grep process | wc -l` && \
   mkdir -p target_dir/{boot,usr/lib} && ln -sv usr/lib target_dir/lib && \
   make CROSS_COMPILE=${CROSS_COMPILE} HOSTCC="${HOSTCC-gcc}" INSTALL_MOD_STRIP=1 INSTALL_MOD_PATH=./target_dir modules_install && \
-  make CROSS_COMPILE=${CROSS_COMPILE} HOSTCC="${HOSTCC-gcc}" INSTALL_PATH=./target_dir/boot install && \
-  ( [[ ${arch} == "mips64" ]] && rm -rf ./target_dir/boot/vmlinux* ) && \
+  make CROSS_COMPILE=${CROSS_COMPILE} HOSTCC="${HOSTCC-gcc}" INSTALL_PATH=./target_dir/boot install || (echo Failed in kernel: $ver && exit -1)
+  [[ ${arch} == "mips64" ]] && rm -rf ./target_dir/boot/vmlinux*
   rm -rfv  ../kernel-$ver-gentoo-cjk-${arch}.tar.xz && \
   tar -pcf ../kernel-$ver-gentoo-cjk-${arch}.tar -C ./target_dir ./ && \
-  xz -z9ev ../kernel-$ver-gentoo-cjk-${arch}.tar || (echo Failed in kernel: $ver && exit -1)
+  xz -z9ev ../kernel-$ver-gentoo-cjk-${arch}.tar
 done
 
 echo Complete built $@.
